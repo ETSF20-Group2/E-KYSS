44b5e62e7ebfc9374462feaa5f1a257e51b669bb
Javier Poremski <javier@poremski.se>
2017-03-15 00:24:54 +0100

    Implementerat Database.java

--- src/ekyss/model/Database.java
+++ src/ekyss/model/Database.java
@@ -1,4 +1,62 @@
 package ekyss.model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import com.mysql.jdbc.Connection;
+
 public class Database {
+
+    private static Database db;
+    private static String url;
+    private static String user;
+    private static String password;
+    private static Connection conn;
+
+    private Database() {
+        url = Do_not_send_up_this_to_GitHub.getUrl();
+        user = Do_not_send_up_this_to_GitHub.getUser();
+        password = Do_not_send_up_this_to_GitHub.getPass();
+        System.out.print("url: " + url + " user: " + user + " pass:" + password);
+    }
+
+    public static Database getInstance() {
+        if (db == null) {
+            db = new Database();
+        }
+        return db;
+    }
+
+    private void open() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = (Connection) DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        if (conn != null) {
+            try {
+                conn.close();
+                conn = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Connection getConnection() {
+        if (conn == null) {
+            open();
+        }
+        return conn;
+    }
+
 }
