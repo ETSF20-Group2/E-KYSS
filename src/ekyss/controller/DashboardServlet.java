f90793380c7fe6539cdc51aa3568d08d173a175e
Javier Poremski <javier@poremski.se>
2017-03-16 00:50:58 +0100

    Inicial implementering av DashboardServlet.

--- src/ekyss/controller/DashboardServlet.java
+++ src/ekyss/controller/DashboardServlet.java
@@ -1,8 +1,13 @@
 package ekyss.controller;
 
 import base.servletBase;
+import ekyss.model.DashboardBean;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(
         name="DashboardServlet",
@@ -13,5 +18,16 @@ import javax.servlet.annotation.WebServlet;
 public class DashboardServlet extends servletBase {
 
     private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        DashboardBean bean = new DashboardBean();
+
+        forwardToView(request, response, "/dashboard.jsp",bean);
+    }
 
 }
