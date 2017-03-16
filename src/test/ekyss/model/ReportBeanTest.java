e053d8c4cb34a42026b1d9014245b4275bac3a66
BC23W <CarlGustafsson23@hotmail.com>
2017-03-16 09:02:18 +0100

    Add files via upload

---
+++ src/test/ekyss/model/ReportBeanTest.java
@@ -0,0 +1,46 @@
+package beanTests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import beans.ReportBean;
+
+public class ReportBeanTest {
+	
+	private ReportBean bean = new ReportBean();
+	
+	@Test
+	public void setUser() throws Exception {
+		 bean.setUser("a");
+		 assertEquals("Inkorrekt värde av user", "a", bean.getUser()); 
+	}
+	
+	@Test
+	public void setGroup() throws Exception {
+		bean.setGroup("a");
+		assertEquals("Inkorrekt värde av group", "a", bean.getGroup());
+	}
+	
+	@Test
+	public void setWeek() throws Exception {
+		bean.setWeek(1);
+		assertEquals("Inkorrekt värde av week", 1, bean.getWeek());
+		bean.setWeek(53);
+		assertFalse("Värdet är större än 52 eller mindre än 1", bean.getWeek() > 0 && bean.getWeek() < 53);	
+	}
+	
+	@Test
+	public void setReportValues() throws Exception {
+		Map<String, Integer> testMap = new HashMap<String, Integer>();
+		testMap.put("a", 10);
+		testMap.put("b", 11);
+		bean.setReportValues(testMap);
+		assertEquals("Map har inte satts till rätt map/värden", bean.getReportValues(), testMap);
+		assertFalse("Map är null", bean.getReportValues() == null);
+	}
+}
---
+++ src/test/ekyss/model/ReportManagementBeanTest.java
@@ -0,0 +1,38 @@
+package beanTests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import beans.ReportManagementBean;
+
+public class ReportManagementBeanTest {
+	
+	private ReportManagementBean bean = new ReportManagementBean();
+	
+	@Test
+	public void setGroup() throws Exception {
+		bean.setGroup("a");
+		assertEquals("Inkorrekt värde av user", "a", bean.getGroup());
+	}
+	
+	@Test
+	public void setSignMap() throws Exception {
+		Map<String, List<Integer>> testMap = new HashMap<String, List<Integer>>();
+		List<Integer> testList = new ArrayList<Integer>();
+		for(int n = 0; n < 5; n++){
+			testList.add(n);
+		}
+		testMap.put("a", testList);
+		bean.setSignMap(testMap);
+		assertEquals("Fel värde av signMap", testMap, bean.getSignMap());
+	}
+	
+	//Behövs fler tester, men fann det svårt att göra rätt test här. Tyckte att koden var lite fel, men nog bara jag som är förvirrad /Calle
+
+}
