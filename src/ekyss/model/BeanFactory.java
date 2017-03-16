9cad28076130bdc87cd73bcb02dd485981a8abc6
Johannes <dat15jsu@student.lu.se>
2017-03-16 13:17:13 +0100

    Signed-off-by: Johannes <dat15jsu@student.lu.se>

--- src/ekyss/model/BeanFactory.java
+++ src/ekyss/model/BeanFactory.java
@@ -8,9 +8,36 @@ public class BeanFactory {
 	}
 	
 	/* LoginServlet */
+	/**
+	 * Returnerar en standardböna av typen LoginBean.
+	 * @return LoginBean
+	 */
+	public static LoginBean getLoginBean() {
+	    LoginBean bean = new LoginBean();
+	    DatabaseHandler db = new DatabaseHandler();
+	    bean.setAllGroups(db.getAllGroupsList());
+	    return bean;
+	}
 	
 	
-	/* GroupManagementServlet */
+	/**
+	 * Kollar om bönan är behörig att logga in i systemet.
+	 * @return LoginBean
+	 */
+	public static void checkLoginBean(LoginBean bean) {
+	    DatabaseHandler db = new DatabaseHandler();
+	
+	    bean.setLogin(
+	        new DatabaseHandler().
+	                loginUser(
+	                    bean.getUsername(),
+	                    bean.getPassword(),
+	                    bean.getSelectedGroup()
+	                )
+	    );
+	}
+	
+	    /* GroupManagementServlet */
 	
 	/**
 	 * Function that is used to fetch a list of all the groups in the database.
