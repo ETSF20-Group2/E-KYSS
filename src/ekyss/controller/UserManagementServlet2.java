c3ee2e3a82d6c39cb7d9ed790e557e9672e524ba
theIDinside <simon.farre.cx@gmail.com>
2017-03-16 22:28:23 +0100

    ladsadasd

---
+++ src/ekyss/controller/UserManagementServlet2.java
@@ -0,0 +1,7 @@
+package ekyss.controller;
+
+/**
+ * Created by cx on 3/16/17.
+ */
+public class UserManagementServlet2 {
+}
---
+++ src/ekyss/model/MailHandler.java
@@ -0,0 +1,7 @@
+package ekyss.model;
+
+/**
+ * Created by cx on 3/16/17.
+ */
+public class MailHandler {
+}
