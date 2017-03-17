e2a356c9822d52775328ff9ec1c1833aedb5ccf9
theIDinside <simon.farre.cx@gmail.com>
2017-03-17 21:29:03 +0100

    Ändrat felstavning i statementfil.

--- src/ekyss/controller/UserManagementServlet.java
+++ src/ekyss/controller/UserManagementServlet.java
@@ -19,30 +19,35 @@ import java.util.Random;
 public class UserManagementServlet extends servletBase {
 
     private static final long serialVersionUID = 1L;
+    private static final long MAXPASSWORDLENGTH = 6;
 
-    protected boolean validateInput(UserManagementBean umb) {
-        return true;
-    }
+    private static final int ACTION_ADDUSER = 1;
+    private static final int ACTION_ASSIGNROLE = 2;
+    private static final int ACTION_ASSIGNGROUP = 3;
 
     protected String generatePassword() {
         final String ALPHANUMERICS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         StringBuilder password = new StringBuilder();
         Random rnd = new Random();
-        while (password.length() < 6) {
+        while (password.length() < MAXPASSWORDLENGTH) {
             int index = (int) (rnd.nextFloat() * ALPHANUMERICS.length());
             password.append(ALPHANUMERICS.charAt(index));
         }
         return password.toString();
     }
 
+    protected boolean validateInput(UserManagementBean umb)
+    {
+        return true;
+    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         UserManagementBean umb = BeanFactory.getUserManagementBean();
         BeanUtilities.populateBean(umb,request);
-        if(validateInput(umb)) {       // TODO: IMPLEMENT validateInput(UserManagementBean)
+        if(validateInput(umb)) { // TODO: IMPLEMENT validateInput(UserManagementBean)
             String pw = generatePassword();
             umb.setPassword(pw);
-            System.out.print(umb    .getUsername());
+            System.out.print(umb.getUsername());
             MailHandler.sendPassword(umb.getEmail(), pw);
             BeanTransaction.addUser(umb);
         }
@@ -53,11 +58,9 @@ public class UserManagementServlet extends servletBase {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        // hantering av flera path gets, dvs om man ska t.ex. tilldela roll / grupp istället.
         UserManagementBean bean = BeanFactory.getUserManagementBean();
-        System.out.println(bean.getAllUsers().toString());
         forwardToView(request, response, "/usermanagement.jsp",bean);
-
     }
 
 }
