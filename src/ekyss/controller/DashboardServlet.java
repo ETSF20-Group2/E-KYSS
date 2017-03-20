fe7d057823b80ab53b9d26a03b3914578455ae28
Javier Poremski <javier@poremski.se>
2017-03-20 22:41:53 +0100

    Uppdatering av Dashboard: PL-vyn

--- src/ekyss/controller/DashboardServlet.java
+++ src/ekyss/controller/DashboardServlet.java
@@ -45,9 +45,9 @@ public class DashboardServlet extends servletBase {
                 bean = new DashboardBean();
             } else if ((boolean) session.getAttribute("ProjectLeader")) {
                 if (request.getParameter("show") != null) {
-                    bean = BeanFactory.getDashboardBeanPL(request.getParameter("show"), (String) session.getAttribute("group"), null);
+                    bean = BeanFactory.getDashboardBeanPL(request.getParameter("show"), (String) session.getAttribute("group"), request.getParameter("user"), request.getParameter("role"), request.getParameter("week"), request.getParameter("stage"));
                 } else {
-                    bean = BeanFactory.getDashboardBeanPL("all", (String) session.getAttribute("group"), null);
+                    bean = BeanFactory.getDashboardBeanPL("all", (String) session.getAttribute("group"), null, null, null, null);
                 }
             } else {
                 bean = BeanFactory.getDashboardBean((String) session.getAttribute("name"), (String) session.getAttribute("group"));
